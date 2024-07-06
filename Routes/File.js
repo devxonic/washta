7106a185873666cf711ca179d295758189e5b8e6
Mubbashir  M.Hussain <79201439+MubbashirHussain@users.noreply.github.com>
2024-07-06 10:11:42 +0500

    Revert "Upload File in AWS"

--- Routes/File.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const FileControllers = require('../controllers/file');
-const middlewares = require('../middlewares');
-
-router.post('/uploadFile' , middlewares.awsUpload.single('file'), FileControllers.uploadFile)
-
-module.exports = router 
