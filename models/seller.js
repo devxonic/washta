bc36d49b50af927a39edfce37d60cd67ffafee0e
syed <ibad.zeez@yahoo.com>
2024-07-15 22:55:38 +0500

    minor fix in business being an object converted to array

--- models/seller.js
+++ models/seller.js
@@ -1,82 +1,87 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
-const SellerSchema = new Schema({
-    username: { type: String, unique: true },
-    fullName: { type: String },
-    password: {
-        type: String,
-        required: true,
-    },
-    email: { type: String },
-    phone: { type: String },
-    business: {
-        uploadDocument: { type: String },
-        companyName: { type: String },
-        location: { type: String },
-        VATNumber: { type: String },
+const SellerSchema = new Schema(
+    {
+        username: { type: String, unique: true },
         fullName: { type: String },
-        position: { type: String },
+        password: {
+            type: String,
+            required: true,
+        },
         email: { type: String },
-        status: { type: String, enum: ["approved", "pending", "rejected"], defualt: "pending" },
-        isApproved: { type: Boolean , defualt  : false }
+        phone: { type: String },
+        business: [
+            {
+                uploadDocument: { type: String },
+                companyName: { type: String },
+                location: { type: String },
+                VATNumber: { type: String },
+                fullName: { type: String },
+                position: { type: String },
+                email: { type: String },
+                status: {
+                    type: String,
+                    enum: ["approved", "pending", "rejected"],
+                    defualt: "pending",
+                },
+                isApproved: { type: Boolean, defualt: false },
+            },
+        ],
+        shops: [{ type: mongoose.Types.ObjectId, ref: "shop" }],
+        isVerifed: { type: Boolean, default: false },
+        sessionKey: {
+            type: String,
+        },
+        notification: {
+            general: {
+                type: Boolean,
+            },
+            sound: {
+                type: Boolean,
+            },
+            vibrate: {
+                type: Boolean,
+            },
+            appUpdates: {
+                type: Boolean,
+            },
+            receiveNotification: {
+                type: String,
+            },
+            doNotReceiveNotifocation: {
+                type: Boolean,
+            },
+        },
+        privacy: {
+            profilePicture: {
+                type: String,
+            },
+            profile: {
+                type: String,
+            },
+            lastSeen: {
+                type: String,
+            },
+            blockedUser: [],
+            sendMessage: {
+                type: String,
+            },
+        },
+        security: {
+            faceId: {
+                type: Boolean,
+            },
+            rememberLoginDetails: {
+                type: Boolean,
+            },
+            touchId: {
+                type: Boolean,
+            },
+        },
     },
-    shops: [{ type: mongoose.Types.ObjectId, ref: 'shop' }],
-    isVerifed: { type: Boolean, default: false },
-    sessionKey: {
-        type: String
-    },
-    notification: {
-        general: {
-            type: Boolean
-        },
-        sound: {
-            type: Boolean
-        },
-        vibrate: {
-            type: Boolean
-        },
-        appUpdates: {
-            type: Boolean
-        },
-        receiveNotification: {
-            type: String
-        },
-        doNotReceiveNotifocation: {
-            type: Boolean
-        }
-    },
-    privacy: {
-        profilePicture: {
-            type: String
-        },
-        profile: {
-            type: String
-        },
-        lastSeen: {
-            type: String
-        },
-        blockedUser: [],
-        sendMessage: {
-            type: String
-        }
-    },
-    security: {
-        faceId: {
-            type: Boolean
-        },
-        rememberLoginDetails: {
-            type: Boolean
-        },
-        touchId: {
-            type: Boolean
-        },
+    { timestamps: true },
+);
 
-    },
-}, { timestamps: true })
-
-
-
-const SellerModel = mongoose.model('seller', SellerSchema);
+const SellerModel = mongoose.model("seller", SellerSchema);
 module.exports = SellerModel;
