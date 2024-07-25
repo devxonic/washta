17ee6b8f0036a7c4d6288499c16c8a505f6ea706
mubbashirhussain <mubbashirhhussain@gmail.com>
2024-07-25 22:36:16 +0500

    revert'pending Status removed in Booking'

--- models/Order.js
+++ models/Order.js
@@ -19,7 +19,7 @@ const OrderSchema = new mongoose.Schema({
     customerId: { type: mongoose.Types.ObjectId, ref: "Customer" },
     vehicleId: { type: mongoose.Types.ObjectId, ref: "vehicle" },
     shopId: { type: mongoose.Types.ObjectId, ref: "shop" },
-    status: { type: String, enum: ["ongoing", "completed", "cancelled"], default: "ongoing" },
+    status: { type: String, enum: ["ongoing", "completed", "pending", "cancelled"], default: "pending" },
     billingStatus: { type: String, enum: ["non-paid", "paid"], default: "non-paid" },
     date: { type: Date, default: Date.now },
     cost: { type: String },
