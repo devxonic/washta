7270b2f69f2205f976e4a8b46858f9b82934a596
mubbashirhussain <mubbashirhhussain@gmail.com>
2024-09-03 23:57:45 +0500

    incomplete

--- controllers/sockethandler.js
+++ controllers/sockethandler.js
@@ -2,6 +2,8 @@ const socketIo = require('socket.io')
 
 
 module.exports = function (server) {
+
+    let joinedUser = { "extra": [] }
     let io = socketIo(server, {
         cors: {
             origin: "*"
@@ -11,7 +13,21 @@ module.exports = function (server) {
         transports: ['websocket', 'polling'],
     });
     io.on('connection', (socket) => {
-        console.log("new user Connected " , socket.id)
+        socket.on("join", (data) => {
+            if (joinedUser[data.chatRoomId] && !joinedUser[data.chatRoomId].includes(data.user.id)) {
+                joinedUser[data.chatRoomId].push(data.user.id)
+                console.log(joinedUser[data.chatRoomId].includes(data.user.id))
+                console.log("join New")
+            } else {
+                console.log("create new room")
+                joinedUser[data.chatRoomId] = [data.user.id]
+            }
+            console.log(`room joined by ${socket.id} roomid => ${data.chatRoomId}`);
+            console.log(joinedUser)
+        })
+        socket.on('send-message-to-care', async (data) => { 
+
+         })
     })
 
 }
