a81081e9fc70dfc5aa3187429ec68c4b6e64ef90
mubbashirhussain <mubbashirhhussain@gmail.com>
2024-09-11 23:50:04 +0500

    cron-job pending

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const http = require('http');
 const socket = require('./controllers/sockethandler');
 const app = express();
 const path = require('path');
+const cron = require('node-cron');
+const shopModel = require('./models/shop');
 
 // socket server 
 const server = http.createServer(app)
@@ -19,6 +21,145 @@ app.use(express.json());
 app.use(cors());
 
 
+
+cron.schedule('* * * * *', async () => {
+    let date = new Date();
+    console.log(date)
+    console.log("cron Job")
+    let shop = await shopModel.aggregate([
+        {
+            '$addFields': {
+                'today': {
+                    '$let': {
+                        'vars': {
+                            'daysOfWeek': [
+                                '', 'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'
+                            ]
+                        },
+                        'in': {
+                            '$arrayElemAt': [
+                                '$$daysOfWeek', {
+                                    '$dayOfWeek': new Date()
+                                }
+                            ]
+                        }
+                    }
+                }
+            }
+        }, {
+            '$project': {
+                'isOpen': 1,
+                'todaySchedule': {
+                    '$arrayElemAt': [
+                        {
+                            '$filter': {
+                                'input': {
+                                    '$objectToArray': '$timing'
+                                },
+                                'as': 'item',
+                                'cond': {
+                                    '$eq': [
+                                        '$$item.k', '$today'
+                                    ]
+                                }
+                            }
+                        }, 0
+                    ]
+                },
+                '_id': 0
+            }
+        }, {
+            '$replaceRoot': {
+                'newRoot': '$todaySchedule.v'
+            }
+        }, {
+            '$addFields': {
+                'openHour': {
+                    '$hour': '$from'
+                },
+                'openMinute': {
+                    '$minute': '$from'
+                },
+                'closeHour': {
+                    '$hour': '$to'
+                },
+                'closeMinute': {
+                    '$minute': '$to'
+                },
+                'currentHour': {
+                    '$hour': new Date()
+                },
+                'currentMinute': {
+                    '$minute': new Date()
+                }
+            }
+        }, {
+            '$addFields': {
+                'isOpened': {
+                    '$cond': {
+                        'if': {
+                            '$eq': [
+                                '$open', false
+                            ]
+                        },
+                        'then': false,
+                        'else': {
+                            '$and': [
+                                {
+                                    '$or': [
+                                        {
+                                            '$gt': [
+                                                '$currentHour', '$openHour'
+                                            ]
+                                        }, {
+                                            '$and': [
+                                                {
+                                                    '$eq': [
+                                                        '$currentHour', '$openHour'
+                                                    ]
+                                                }, {
+                                                    '$gte': [
+                                                        '$currentMinute', '$openMinute'
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }, {
+                                    '$or': [
+                                        {
+                                            '$lt': [
+                                                '$currentHour', '$closeHour'
+                                            ]
+                                        }, {
+                                            '$and': [
+                                                {
+                                                    '$eq': [
+                                                        '$currentHour', '$closeHour'
+                                                    ]
+                                                }, {
+                                                    '$lte': [
+                                                        '$currentMinute', '$closeMinute'
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    }
+                }
+            }
+        }, {
+            '$merge': {
+                'into': 'shops',
+                'whenMatched': 'merge',
+                'whenNotMatched': 'discard'
+            }
+        }
+    ])
+});
 
 // api routing
 app.use('/api', require('./Routes/File'));
